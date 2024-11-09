769761414a53c4b9767bf35ae8d586a1a44481de
luccasmedina <gianluccas54@gmail.com>
2024-11-09 00:44:04 -0300

    Incluindo controller,mode, routes e schema para Pets

---
+++ src/controllers/petsController.js
@@ -0,0 +1,57 @@
+import {
+    findAllPets,
+    findPetById,
+    createPet,
+    updatePet,
+    deletePet,
+} from "../models/petsModel.js";
+
+export const getAllPets = async (req, res) => {
+    try {
+        const pets = await findAllPets();
+        return res.status(200).json(pets);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+export const getPetById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const pet = await findPetById(id);
+        return res.status(200).json(pet);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+export const createPet = async (req, res) => {
+    const { nome, idade, descricao, status} = req.body;
+    try {
+        const pet = await createPet({ nome, idade, descricao, status});
+        return res.status(201).json(pet);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+export const updatePet = async (req, res) => {
+    const { id } = req.params;
+    const { nome, idade, descricao, status} = req.body;
+    try {
+        const pet = await updatePet(id, { nome, idade, descricao, status });
+        return res.status(200).json(pet);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+export const deletePet = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await deletePet(id);
+        return res.status(200).json({ message: "Pet deleted successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import adotantesRaoutes from './adotantesRoutes.js';
+import petsRoutes from './petsRoutes.js';
 
 const indexRouter = express.Router();
 
 indexRouter.use('/usuario', adotantesRaoutes);
+indexRouter.use('/pet', petsRoutes);
 
 export default indexRouter;
